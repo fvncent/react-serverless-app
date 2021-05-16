ad6d0c6a09a5e0e596b5b44bc698aa6ef4ba2f6a
Vincent Ferrer <vincent@smedia.com.au>
2021-05-16 22:19:06 +0800

    styled game page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { StyledCharacter, Styledgame, StyledScore, StyledTimer } from '../styled/Game'
+import { Strong } from '../styled/Random'
 
 function Game() {
     return (
-        <div>
-            <h1>Game</h1>
-        </div>
+        <Styledgame>
+            <StyledScore>Score: <Strong>0</Strong></StyledScore>
+            <StyledCharacter>A</StyledCharacter>
+            <StyledTimer>Time: <Strong>00:000</Strong> </StyledTimer>
+        </Styledgame>
     )
 }
 
